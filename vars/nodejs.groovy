954540ca81f5344e04b71aef234015c7c769dacb
mvpc-11232 <mvp2612@hotmail.com>
2022-10-04 07:13:20 +0530

    Auto Push Commit

---
+++ vars/nodejs.groovy
@@ -0,0 +1,33 @@
+def lintCheck() {
+    sh ''' 
+         # We want Devs to handle the lint checks failure 
+         # npm i jslint 
+         # node_modules/jslint/bin/jslint.js  server.js || true 
+         echo Starting lint checks
+         echo Lint Checks Completed
+    ''' 
+}
+
+def call() {
+    pipeline {
+        agent any 
+        stages {
+            
+            stage('Downloading the dependencies') {
+                steps {
+                    sh "npm install"
+                }
+            }
+
+            stage('Lint Checks') {
+                steps {
+                    script { 
+                        nodejs.lintChecks()
+                    }
+                }
+            }
+        }    // end of statges 
+    }
+}
+
+// call is the default function which will be called when you call the fileName 
