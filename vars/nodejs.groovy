199d4670869b0c947bcb090a843687c0664aa9e1
mvpc-11232 <mvp2612@hotmail.com>
2022-10-06 07:04:58 +0530

    Updating the Jenkinsfile Message

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -10,7 +10,7 @@ def lintCheck() {
 
 def sonarCheck() {
     sh ''' 
-        sonar-scanner -Dsonar.host.url=http://172.31.0.99:9000 -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT}
+        sonar-scanner -Dsonar.host.url=http://172.31.0.99:9000 -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin -Dsonar.password=password
     ''' 
 }
 
