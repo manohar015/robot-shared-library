860754e53b37f4446e99ced429e392b4cbbdd46f
mvpc-11232 <mvp2612@hotmail.com>
2022-10-07 23:48:29 +0530

    Auto Push Commit

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,6 +1,6 @@
 def sonarCheck() {
     sh ''' 
-        sonar-scanner -Dsonar.host.url=http://172.31.0.99:9000 -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}
+        sonar-scanner -Dsonar.host.url=http://172.31.0.99:9000 -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} ${ARGS}
         sonar-quality-gate ${SONAR_USR} ${SONAR_PSW} 172.31.0.99 ${COMPONENT} // This gives the result of the scan and based on that either it will abort the pipeline or will proceed further.
         echo sonarchecks for ${COMPONENT}
       ''' 
