0d0582f40ce9907e9a937dd88b0417174c783b49
mvpc-11232 <mvp2612@hotmail.com>
2022-10-11 06:30:54 +0530

    Updating the Jenkinsfile Message

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -79,7 +79,7 @@ def call() {
                 }
                 steps {
                     sh ''' 
-                        curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file pom.xml http://172.31.0.75:8081/repository/${COMPONENT}/${COMPONENT}.zip
+                        curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://172.31.0.75:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
                     '''
                 }
             }
