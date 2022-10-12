b22dcf8bb4dd8bccfd34ab5fbcaa8c4881c0e15e
mvpc-11232 <mvp2612@hotmail.com>
2022-10-12 06:06:49 +0530

    Auto Push Commit

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,3 +1,14 @@
+def lintCheck() {
+    sh ''' 
+         # We want Devs to handle the lint checks failure 
+         # npm i jslint 
+         # node_modules/jslint/bin/jslint.js  server.js || true 
+         echo Starting lint checks
+         echo Lint Checks Completed for ${COMPONENT}
+    ''' 
+}
+
+
 def sonarCheck() {
     sh ''' 
         sonar-scanner -Dsonar.host.url=http://172.31.0.99:9000 -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} ${ARGS}
--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,12 +1,5 @@
-def lintCheck() {
-    sh ''' 
-         # We want Devs to handle the lint checks failure 
-         # npm i jslint 
-         # node_modules/jslint/bin/jslint.js  server.js || true 
-         echo Starting lint checks
-         echo Lint Checks Completed for ${COMPONENT}
-    ''' 
-}
+
+
 
 def call() {
     node {
