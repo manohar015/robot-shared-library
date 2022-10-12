7e033d5bbaad66cd7f72f5415922ae322d18d09f
mvpc-11232 <mvp2612@hotmail.com>
2022-10-12 07:05:34 +0530

    Auto Push Commit

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -13,51 +13,51 @@ def call() {
 }
 
 
-def call() {
-    pipeline {
-        agent any 
-        environment {
-            SONAR      = credentials('SONAR')
-            NEXUS      = credentials('NEXUS')
-        }
-        stages {
-            stage('Lint Check') {
-                steps {
-                    script { 
-                        lintCheck()
-                    }
-                }
-            }
-            stage('Sonar Check') {
-                steps {
-                    script { 
-                        sh "mvn clean compile"
-                        env.ARGS="-Dsonar.java.binaries=target/"
-                        common.sonarCheck()
-                    }
-                }
-            }
-           stage('Test Cases') {
-            parallel {
-                stage('Unit Tests') {
-                    steps {
-                        sh 'echo Unit Test Cases Completed'
-                         }
-                    }
-                stage('Integration Tests') {
-                    steps {
-                        sh 'echo Integration Test Cases Completed'
-                         }
-                    }
-                stage('Functional Tests') {
-                    steps {
-                        sh 'echo Functional Test Cases Completed'
-                         }
-                    }
-                }
-            }
-        }    // end of statges 
-    }
-}
+// def call() {
+//     pipeline {
+//         agent any 
+//         environment {
+//             SONAR      = credentials('SONAR')
+//             NEXUS      = credentials('NEXUS')
+//         }
+//         stages {
+//             stage('Lint Check') {
+//                 steps {
+//                     script { 
+//                         lintCheck()
+//                     }
+//                 }
+//             }
+//             stage('Sonar Check') {
+//                 steps {
+//                     script { 
+//                         sh "mvn clean compile"
+//                         env.ARGS="-Dsonar.java.binaries=target/"
+//                         common.sonarCheck()
+//                     }
+//                 }
+//             }
+//            stage('Test Cases') {
+//             parallel {
+//                 stage('Unit Tests') {
+//                     steps {
+//                         sh 'echo Unit Test Cases Completed'
+//                          }
+//                     }
+//                 stage('Integration Tests') {
+//                     steps {
+//                         sh 'echo Integration Test Cases Completed'
+//                          }
+//                     }
+//                 stage('Functional Tests') {
+//                     steps {
+//                         sh 'echo Functional Test Cases Completed'
+//                          }
+//                     }
+//                 }
+//             }
+//         }    // end of statges 
+//     }
+// }
 
 // call is the default function which will be called when you call the fileName 
--- vars/python.groovy
+++ vars/python.groovy
@@ -1,60 +1,64 @@
-
 def call() {
     node {
+        git branch: 'main', url: "https://github.com/b50-clouddevops/${COMPONENT}.git"
         env.APPTYPE="python"
         common.lintCheck()
         env.ARGS="-Dsonar.sources=."
         common.sonarCheck()
-    }
-}
-
-
-
-def call() {
-    pipeline {
-        agent any 
-        environment {
-            SONAR      = credentials('SONAR')
-            NEXUS      = credentials('NEXUS')
+        common.testCases()
+        if (env.TAG_NAME != null) {
+            common.artifact()
         }
-        stages {
-            stage('Lint Check') {
-                steps {
-                    script { 
-                        lintCheck()
-                    }
-                }
-            }
-            stage('Sonar Check') {
-                steps {
-                    script { 
-                        env.ARGS="-Dsonar.sources=."
-                        common.sonarCheck()
-                    }
-                }
-            }
-           stage('Test Cases') {
-            parallel {
-                stage('Unit Tests') {
-                    steps {
-                        sh 'echo Unit Test Cases Completed'
-                         }
-                    }
-                stage('Integration Tests') {
-                    steps {
-                        sh 'echo Integration Test Cases Completed'
-                         }
-                    }
-                stage('Functional Tests') {
-                    steps {
-                        sh 'echo Functional Test Cases Completed'
-                         }
-                    }
-                }
-            }
-        }    // end of statges 
     }
 }
+
+
+
+// def call() {
+//     pipeline {
+//         agent any 
+//         environment {
+//             SONAR      = credentials('SONAR')
+//             NEXUS      = credentials('NEXUS')
+//         }
+//         stages {
+//             stage('Lint Check') {
+//                 steps {
+//                     script { 
+//                         lintCheck()
+//                     }
+//                 }
+//             }
+//             stage('Sonar Check') {
+//                 steps {
+//                     script { 
+//                         env.ARGS="-Dsonar.sources=."
+//                         common.sonarCheck()
+//                     }
+//                 }
+//             }
+//            stage('Test Cases') {
+//             parallel {
+//                 stage('Unit Tests') {
+//                     steps {
+//                         sh 'echo Unit Test Cases Completed'
+//                          }
+//                     }
+//                 stage('Integration Tests') {
+//                     steps {
+//                         sh 'echo Integration Test Cases Completed'
+//                          }
+//                     }
+//                 stage('Functional Tests') {
+//                     steps {
+//                         sh 'echo Functional Test Cases Completed'
+//                          }
+//                     }
+//                 }
+//             }
+//         }    // end of statges 
+//     }
+// }
 
 // Reference for pylint 
 // https://pypi.org/project/pylint/
