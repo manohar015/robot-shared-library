f0f4a75aacbfe566bf334a73cdca7f95fe2fa08c
mvpc-11232 <mvp2612@hotmail.com>
2022-10-12 07:17:55 +0530

    Updating the Jenkinsfile Message

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,9 +1,12 @@
 def sonarCheck() {
+   stage('Sonar Checks') {
     sh ''' 
         # sonar-scanner -Dsonar.host.url=http://172.31.0.99:9000 -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} ${ARGS}
         # sonar-quality-gate.sh ${SONAR_USR} ${SONAR_PSW} 172.31.0.99 ${COMPONENT} || true
         echo sonarchecks for ${COMPONENT}
-      ''' 
+        
+      '''
+   } 
 }
 
 
