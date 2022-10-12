1858a1ee200e2bb5b1b9e841f859f0a6e04f425b
mvpc-11232 <mvp2612@hotmail.com>
2022-10-12 06:37:00 +0530

    Auto Push Commit

--- vars/common.groovy
+++ vars/common.groovy
@@ -55,43 +55,43 @@ def testCases() {
 
 
 def artifact {
-//             stage('Check the release') {
-//                 when {
-//                     expression { env.TAG_NAME != null }   // Only runs when you run this against the TAG
-//                 }
-//                 steps {
-//                     script {
-//                         env.UPLOAD_STATUS=sh(returnStdout: true, script: 'curl -L -s http://172.31.0.75:8081/service/rest/repository/browse/${COMPONENT} | grep ${COMPONENT}-${TAG_NAME}.zip || true')
-//                         print UPLOAD_STATUS
-//                     }
-//                 }
-//             }
+      stage('Check the release') {
+            when {
+                expression { env.TAG_NAME != null }   // Only runs when you run this against the TAG
+            }
+             steps {
+                script {
+                    env.UPLOAD_STATUS=sh(returnStdout: true, script: 'curl -L -s http://172.31.0.75:8081/service/rest/repository/browse/${COMPONENT} | grep ${COMPONENT}-${TAG_NAME}.zip || true')
+                  print UPLOAD_STATUS
+                 }
+             }
+      }
 
-//             stage('Prepare Artifacts') {
-//                 when {
-//                     expression { env.TAG_NAME != null }   // Only runs when you run this against the TAG
-//                     expression { env.UPLOAD_STATUS == "" }
-//                 }
-//                 steps {
-//                     sh ''' 
-//                         npm install 
-//                         zip ${COMPONENT}-${TAG_NAME}.zip node_modules server.js
+            stage('Prepare Artifacts') {
+                when {
+                    expression { env.TAG_NAME != null }   // Only runs when you run this against the TAG
+                    expression { env.UPLOAD_STATUS == "" }
+                }
+                steps {
+                    sh ''' 
+                        npm install 
+                        zip ${COMPONENT}-${TAG_NAME}.zip node_modules server.js
 
-//                     ''' 
-//                 }
-//             }
+                    ''' 
+                }
+            }
 
-//             stage('Upload Artifacts') {
-//                 when {
-//                     expression { env.TAG_NAME != null }   // Only runs when you run this against the TAG
-//                     expression { env.UPLOAD_STATUS == "" }
-//                 }
-//                 steps {
-//                     sh ''' 
-//                         curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://172.31.0.75:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
-//                     '''
-//                 }
-//             }
+            stage('Upload Artifacts') {
+                when {
+                    expression { env.TAG_NAME != null }   // Only runs when you run this against the TAG
+                    expression { env.UPLOAD_STATUS == "" }
+                }
+                steps {
+                    sh ''' 
+                        curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://172.31.0.75:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
+                    '''
+                }
+            }
 }
 
 // def testCases() {
