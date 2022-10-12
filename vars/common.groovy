18ce0d49bec0f8fca87bcf28acee40fd20ae3341
mvpc-11232 <mvp2612@hotmail.com>
2022-10-12 07:17:02 +0530

    Updating the Jenkinsfile Message

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,3 +1,12 @@
+def sonarCheck() {
+    sh ''' 
+        # sonar-scanner -Dsonar.host.url=http://172.31.0.99:9000 -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} ${ARGS}
+        # sonar-quality-gate.sh ${SONAR_USR} ${SONAR_PSW} 172.31.0.99 ${COMPONENT} || true
+        echo sonarchecks for ${COMPONENT}
+      ''' 
+}
+
+
 def lintCheck() {
   stage('Lint Checks') {
 
@@ -124,13 +133,6 @@ def artifact() {
 //             }
 // }
 
-def sonarCheck() {
-    sh ''' 
-        # sonar-scanner -Dsonar.host.url=http://172.31.0.99:9000 -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} ${ARGS}
-        # sonar-quality-gate.sh ${SONAR_USR} ${SONAR_PSW} 172.31.0.99 ${COMPONENT} || true
-        echo sonarchecks for ${COMPONENT}
-      ''' 
-}
 
 
 // For non-Java, code source partamter is  -Dsonar.sources=. 
