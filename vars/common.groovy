8afdef8bc049a26d46346834a0f7b77055535d82
mvpc-11232 <mvp2612@hotmail.com>
2022-10-12 06:33:21 +0530

    Updating the Jenkinsfile Message

--- vars/common.groovy
+++ vars/common.groovy
@@ -53,6 +53,21 @@ def testCases() {
     }
 }
 
+// def testCases() {
+//     stage('Test Cases') {
+//           stage('Unit Tests') {
+//                sh 'echo Unit Test Cases Completed'
+//               }    
+//           stage('Integration Tests') {
+//                 sh 'echo Integration Test Cases Completed'
+//               }
+                  
+//           stage('Functional Tests') {
+//                 sh 'echo Functional Test Cases Completed'      
+//                  }
+//             }
+// }
+
 def sonarCheck() {
     sh ''' 
         # sonar-scanner -Dsonar.host.url=http://172.31.0.99:9000 -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} ${ARGS}
