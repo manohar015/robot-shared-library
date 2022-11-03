0419bc6047c9ea24f0c97921fb52a086246ee6ed
mvpc-11232 <mvp2612@hotmail.com>
2022-11-03 06:08:15 +0530

    Auto Push Commit

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,45 +1,50 @@
-def call() {
-    pipeline {
-        agent any 
-        environment {
-            SONAR      = credentials('SONAR')
-            NEXUS      = credentials('NEXUS')
-        }
-        stages {
-            stage('Lint Check') {
-                steps {
-                    script { 
-                        lintCheck()
-                    }
-                }
-            }
-            stage('Sonar Check') {
-                steps {
-                    script { 
-                        env.ARGS="-Dsonar.sources=."
-                        common.sonarCheck()
-                    }
-                }
-            }
-           stage('Test Cases') {
-            parallel {
-                stage('Unit Tests') {
-                    steps {
-                        sh 'echo Unit Test Cases Completed'
-                         }
-                    }
-                stage('Integration Tests') {
-                    steps {
-                        sh 'echo Integration Test Cases Completed'
-                         }
-                    }
-                stage('Functional Tests') {
-                    steps {
-                        sh 'echo Functional Test Cases Completed'
-                         }
-                    }
-                }
-            }
-        }    // end of statges 
-    }
+def  call() {
+    
 }
+
+
+// def call() {
+//     pipeline {
+//         agent any 
+//         environment {
+//             SONAR      = credentials('SONAR')
+//             NEXUS      = credentials('NEXUS')
+//         }
+//         stages {
+//             stage('Lint Check') {
+//                 steps {
+//                     script { 
+//                         lintCheck()
+//                     }
+//                 }
+//             }
+//             stage('Sonar Check') {
+//                 steps {
+//                     script { 
+//                         env.ARGS="-Dsonar.sources=."
+//                         common.sonarCheck()
+//                     }
+//                 }
+//             }
+//            stage('Test Cases') {
+//             parallel {
+//                 stage('Unit Tests') {
+//                     steps {
+//                         sh 'echo Unit Test Cases Completed'
+//                          }
+//                     }
+//                 stage('Integration Tests') {
+//                     steps {
+//                         sh 'echo Integration Test Cases Completed'
+//                          }
+//                     }
+//                 stage('Functional Tests') {
+//                     steps {
+//                         sh 'echo Functional Test Cases Completed'
+//                          }
+//                     }
+//                 }
+//             }
+//         }    // end of statges 
+//     }
+// }
