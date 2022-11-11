6f46ca152e7864c7a049a109c61ccf62b9b2dca5
mvpc-11232 <mvp2612@hotmail.com>
2022-11-11 06:52:11 +0530

    Auto Push Commit

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -29,14 +29,15 @@ properties([
             sh ''' 
                 cd ${TERRAFORM_DIR}
                 export TF_VAR_APP_VERSION=${APP_VERSION}
-                terraform plan -var-file=env-${ENV}/${ENV}.tfvars -var APP_VERSION=${APP_VERSION}
+                terraform plan -var-file=env-${ENV}/${ENV}.tfvars
             '''
         }  
 
         stage('Terraform Apply'){
             sh ''' 
                 cd ${TERRAFORM_DIR}
-                terraform ${ACTION} -var-file=env-${ENV}/${ENV}.tfvars -auto-approve -var APP_VERSION=${APP_VERSION}
+                export TF_VAR_APP_VERSION=${APP_VERSION}
+                terraform ${ACTION} -var-file=env-${ENV}/${ENV}.tfvars -auto-approve
             '''
             }            
         }
